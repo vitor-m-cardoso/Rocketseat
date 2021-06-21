faa075b60ff1d974a538c3313eed16c3012e08a5
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-06-21 01:28:22 -0300

    iniciando projeto LetMeAsk utilizando React e TypeScript :rocket:

---
+++ letmeask/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/Button'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
